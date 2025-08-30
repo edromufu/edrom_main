e710370359004575b294d1d30a03049b410441a4
victorcaixet <victorvasconcelos676@gmail.com>
2025-08-29 22:33:21 -0300

    Add: Camera adcionada e publicando no tópico

--- src/edrom_simulation/aurea_webots/aurea_webots/aurea_sensors.py
+++ src/edrom_simulation/aurea_webots/aurea_webots/aurea_sensors.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Image 
 from controller import Robot
 
 '''
@@ -8,7 +9,7 @@ Controller que se conecta com o Webots, lê os dados dos sensores e publica em t
 '''
 
 class AureaSensors:
-    def __init__(self, webots_node, properties):
+    def init(self, webots_node, properties):
 
         # Inicializa o ROS2
         rclpy.init(args=None)
@@ -35,14 +36,14 @@ class AureaSensors:
             return
         self.__gyro.enable(self.__timestep)
 
-        '''
+        
         # Inicializa a câmera
         self.__camera = self.__robot.getDevice("camera_front")
         if self.__camera is None:
             self.__node.get_logger().error("Câmera not found.")
             return
         self.__camera.enable(self.__timestep)
-        '''
+        
         # Cria publisher
         self.__accel_pub = self.__node.create_publisher(Vector3, '/imu/accel', 10)
         self.__node.get_logger().info("Accel publishing in /imu/accel.")
@@ -50,21 +51,20 @@ class AureaSensors:
         self.__gyro_pub = self.__node.create_publisher(Vector3, '/imu/gyro', 10)
         self.__node.get_logger().info("Gyro publishing in /imu/gyro")
 
-        '''
+        
         self.__camera_pub = self.__node.create_publisher(Image, '/camera/image', 10)
         self.__node.get_logger().info("Câmera publishing in /camera/image.")
-        '''
+        
     def step(self):
         """
         Esta função é chamada repetidamente em cada passo da simulação.
         """
         # Processa callbacks ROS2
         rclpy.spin_once(self.__node, timeout_sec=0)
-        '''
+        
         if self.__camera is not None:
-            self.camera.enable(self.__timestep)
-            image = camera.getImage()
-            if image is not None:   
+            image = self.__camera.getImage()
+            if image is not None:
                 msg = Image()
                 msg.data = image
                 msg.height = self.__camera.getHeight()
@@ -73,7 +73,7 @@ class AureaSensors:
                 msg.is_bigendian = 0
                 msg.step = msg.width * 4
                 self.__camera_pub.publish(msg)
-        '''
+        
         if self.__accel is not None:
             values = self.__accel.getValues()
             msg = Vector3()
